5303f4e4ccd86a4cf9408e2e60fe9023c95404ad
alegrelias26 <elias.n.alegre26-1@gmail.com>
2026-04-15 15:43:11 -0300

    Se implementan test unitarios del metodo 'sqrt'

--- tests/test_sqrt.py
+++ tests/test_sqrt.py
@@ -5,20 +5,18 @@ import pytest
 from src.calculator import sqrt
 
 
-# --- EJEMPLO (no borrar) ---
 def test_sqrt_cuadrado_perfecto():
     """Ejemplo: la raíz de 9 debe dar 3.0."""
     assert sqrt(9) == 3.0
 
 
-# --- TU TURNO ---
-# Agregá tests para los siguientes casos:
 #   - Raíz de 0 (resultado: 0.0)
+def test_raiz_cero():
+    assert sqrt(0) == 0
 #   - Raíz de un número que no es cuadrado perfecto (resultado decimal)
+def test_raiz_inexacta():
+    assert type(sqrt(2)) == float
 #   - Raíz de un número negativo → debe lanzar ValueError
-#
-# Pista: para testear excepciones usá pytest.raises:
-#
-# def test_sqrt_negativo():
-#     with pytest.raises(ValueError):
-#         sqrt(-4)
+def test_numero_imaginario():
+    with pytest.raises(ValueError):
+        sqrt(-4)
