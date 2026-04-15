537feef2f7855ccbf179f13bd754ee2031273f49
alegrelias26 <elias.n.alegre26-1@gmail.com>
2026-04-15 15:47:49 -0300

    Se implementan pruebas unitarias para el metodo 'sub'

--- tests/test_sub.py
+++ tests/test_sub.py
@@ -5,17 +5,20 @@ import pytest
 from src.calculator import sub
 
 
-# --- EJEMPLO (no borrar) ---
 def test_sub_resta_positivos():
     """Ejemplo: 5 - 2 debe dar 3."""
     assert sub(5, 2) == 3
 
 
-# --- TU TURNO ---
-# Agregá tests para los siguientes casos:
 #   - Restar un número mayor al primero (resultado negativo)
+def test_suma_mayor_menor():
+    assert sub(2, 5) == -3
 #   - Restar cero
+def test_resta_cero():
+    assert sub(2, 0) == 2
 #   - Restar dos números negativos
+def test_resta_negativa():
+    assert sub(-2, -2) == 0
 #   - Restar dos números decimales (float)
-#
-# Pista: podés usar @pytest.mark.parametrize para probar varios casos a la vez.
+def test_resta_decimal():
+    assert sub(2.5, 1) == 1.5
