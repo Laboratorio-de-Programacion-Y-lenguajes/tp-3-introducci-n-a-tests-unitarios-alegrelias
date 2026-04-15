9758af30bd1aed41b0073d6cf14bd837087b60b5
alegrelias26 <elias.n.alegre26-1@gmail.com>
2026-04-15 15:20:04 -0300

    Se implementan los test para la funcion 'suma'

--- tests/test_add.py
+++ tests/test_add.py
@@ -4,27 +4,20 @@ import pytest
 
 from src.calculator import add
 
-
-# --- EJEMPLO (no borrar) ---
 def test_add_suma_positivos():
     """Ejemplo: 1 + 2 debe dar 3."""
     assert add(1, 2) == 3
 
 
-# --- TU TURNO ---
-# Agregá tests para los siguientes casos:
 #   - Sumar dos números negativos
+def test_add_suma_negativa():
+    assert add(-1, -1) == -2
 #   - Sumar un número positivo y uno negativo
+def test_add_positivo_negativo():
+    assert add(-1, 8) == 7
 #   - Sumar con cero
+def test_add_suma_cero():
+    assert add(1, 0) == 1
 #   - Sumar dos números decimales (float)
-#
-# Pista: podés usar @pytest.mark.parametrize para probar varios casos a la vez.
-#
-# Ejemplo de test parametrizado:
-#
-# @pytest.mark.parametrize("a,b,expected", [
-#     (..., ..., ...),
-#     (..., ..., ...),
-# ])
-# def test_add_parametrizado(a, b, expected):
-#     assert add(a, b) == expected
+def test_add_suma_decimal():
+    assert add(2.5, 2.5) == 5.0
